7f30f5045a8e0dffc1902256623e2f7c8baf239f
Replit user <>
2023-08-27 00:25:14 +0000

    express object methods

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
 let express = require('express');
 let app = express();
+let path = require('path'); // Import the path module
+
+// Mount the express.static() middleware to serve assets from the 'public' folder using app.use method
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Define a route using the app.get() method
 app.get('/', function(req, res) {
-  res.send('Hello Express');
+  // Use the path module to get the absolute path of the HTML file
+  let absolutePath = path.join(__dirname, 'views', 'index.html');
+
+  // Send the HTML file as a response
+  res.sendFile(absolutePath);
 });
 
 module.exports = app; // Export the Express app
